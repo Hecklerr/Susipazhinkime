9b4897decc9dea83561e45161fbc305e8adb60bf
abaranouski <abaranouski@eisgroup.com>
2022-05-08 21:28:30 +0300

    fixed link issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import './App.css';
 import {GetStarted} from './containers/GetStarted'
 import {Learning} from "./containers/Learninig";
 import {Test} from './containers/Test'
+import {PageLayout} from "./containers/PageLayout";
 
 function App() {
   return (
     <div className="App">
         <Router>
             <Routes>
-                <Route path="/" element={<GetStarted />} />
-                <Route path="test/:id" element={<Test />} />
-                <Route path="learn/:id" element={<Learning />} />
-                <Route path="*" element={<GetStarted />} />
+                <Route path="/Susipazhinkime" element={<PageLayout />}>
+                    <Route index element={<GetStarted />} />
+                    <Route path="test/:id" element={<Test />} />
+                    <Route path="learn/:id" element={<Learning />} />
+                </Route>
+                <Route path="*" element={<Navigate replace to="/Susipazhinkime" />} />
             </Routes>
         </Router>
     </div>
